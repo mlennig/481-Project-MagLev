b607b6a9ea5200ae50daa2aaa34ec8dc83aa7750
Mim <miriam.lennig@gmail.com>
2018-12-13 18:07:18 -0500

    Save Appendix A Figures Add OCF, CCF

--- MagLev_PIDDesign.m
+++ MagLev_PIDDesign.m
@@ -7,3 +7,28 @@
 % 4. Step Response
 % 5. Square Wave Response
 % 6. Sinusoidal Response 
+
+
+% SISO, Location #1, Linearized Actuator, Linearized Sensor
+% p.133 3a)
+A = [0 1; 0 0];
+B = [0; 826];
+C = [1 0];
+
+% Create state-space model 
+ss_ol = ss(A,B,C,0);
+
+% Convert state-space representation to transfer function
+[num,denom]= ss2tf(A,B,C,0)
+tf_ol = tf(num,denom)
+
+% Find poles of open-loop system
+% by finding the eigenvalues of the 
+% system matrix A. These poles are
+% equal to the poles of the transfer
+% function, and determine stability. 
+% det(sI - A) = 0
+poles = eig(A)
+
+% 6. Design a PID Controller 
+sisotool(ss_ol)
