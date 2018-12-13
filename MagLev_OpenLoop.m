17bb77158952bdad0377e164fa6ee5da5ca5076d
Mim <miriam.lennig@gmail.com>
2018-12-13 18:07:18 -0500

    Save Appendix A Figures Add OCF, CCF

--- MagLev_OpenLoop.m
+++ MagLev_OpenLoop.m
@@ -29,16 +29,53 @@ tf_ol = tf(num,denom)
 poles = eig(A)
 
 % 3. O.C.F, C.C.F, J.C.F. 
+% OCF
 % Compute the observability matrix
-Ob = obsv(A,C)
+Ox = obsv(A,C)
 % Determine the number of unobservable states
-unob = length(A)-rank(Ob)
-```````````````
+nUnobservableStates = length(A)-rank(Ox)
+
+% Determine characteristic polynomial
+syms x
+CP = charpoly(A,x)
+
+% Observable Canonical Form
+Ao = [0 0; 1 0]
+Co = [0 1]
+
+Oxo = obsv(Ao, Co)
+Toinv = inv(Oxo) * Ox
+Bo = Toinv * B
+To = inv(Toinv)
+
+% Check equality 
+Ao
+Toinv * A * To
+
+% Observable Forms in State Space Representation
+ss_ol_obs = ss(Ao,Bo,Co,0);
 
 % Compute the controllability matrix 
-Co = ctrb(A,B)
+Cx = ctrb(A,B)
 % Determine the number of uncontrollable states
-unco = length(A) - rank(Co)
+unco = length(A) - rank(Cx)
+
+% Controllable Canonical Form
+Ac = Ao.'
+Bc = Co.'
+
+Cxc = ctrb(Ac, Bc)
+Tcinv = inv(Cxc) * Cx
+
+Tc = inv(Tcinv)
+Cc = C * Tc
+
+% Check equality 
+Ac
+Tcinv * A * Tc
+
+% Controllable Forms in State Space Representation
+ss_ol_ctrl = ss(Ac,Bc,Cc,0);
 
 % Create Jordan form of matrix A
 JA = jordan(A)
@@ -54,6 +91,7 @@ title('Impulse Reponse of Open-Loop Linearized Magnetic Levitation System');
 subplot(2,1,2)
 step(ss_ol)
 title('Step Reponse of Open-Loop Linearized Magnetic Levitation System');
+
 
 % 5. Bode Plot & Root Locus 
 % Bode Plot
